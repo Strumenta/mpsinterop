7a5717451dbb18b273f364de0ca95de38ee48d4b
Federico Tomassetti <federico@tomassetti.me>
2019-08-04 12:06:46 +0200

    testing Node children

--- mpsinterface/src/test/kotlin/com/strumenta/mpsinterop/logicalmodel/NodeTest.kt
+++ mpsinterface/src/test/kotlin/com/strumenta/mpsinterop/logicalmodel/NodeTest.kt
@@ -231,4 +231,64 @@ class NodeTest {
         node1.addChild(link, node3)
         assertEquals(listOf(node2, node3), node1.children)
     }
+
+    @Test
+    fun numberOfChildren() {
+        val language = Language(UUID.randomUUID(), "MyLanguage")
+
+        val concept1 = language.addConcept(1234234L, "MyConcept")
+        val link = concept1.addContainmentLink(2323L, "MyLink", Multiplicity.ZERO_OR_MORE)
+
+        val node1 = Node(concept1, NodeId.regular(124L))
+        val node2 = Node(concept1, NodeId.regular(125L))
+        val node3 = Node(concept1, NodeId.regular(126L))
+
+        assertEquals(0, node1.numberOfChildren)
+        node1.addChild(link, node2)
+        assertEquals(1, node1.numberOfChildren)
+        node1.addChild(link, node3)
+        assertEquals(2, node1.numberOfChildren)
+    }
+
+    @Test
+    fun getChildren() {
+        val language = Language(UUID.randomUUID(), "MyLanguage")
+
+        val concept1 = language.addConcept(1234234L, "MyConcept")
+        val link = concept1.addContainmentLink(2323L, "MyLink", Multiplicity.ZERO_OR_MORE)
+
+        val node1 = Node(concept1, NodeId.regular(124L))
+        val node2 = Node(concept1, NodeId.regular(125L))
+        val node3 = Node(concept1, NodeId.regular(126L))
+
+        assertEquals(emptyList(), node1.getChildren(link))
+        node1.addChild(link, node2)
+        assertEquals(listOf(node2), node1.getChildren(link))
+        node1.addChild(link, node3)
+        assertEquals(listOf(node2, node3), node1.getChildren(link))
+    }
+
+    @Test
+    fun hasChild() {
+        val language = Language(UUID.randomUUID(), "MyLanguage")
+
+        val concept1 = language.addConcept(1234234L, "MyConcept")
+        val link = concept1.addContainmentLink(2323L, "MyLink", Multiplicity.ZERO_OR_MORE)
+
+        val node1 = Node(concept1, NodeId.regular(124L))
+        val node2 = Node(concept1, NodeId.regular(125L))
+        val node3 = Node(concept1, NodeId.regular(126L))
+
+        assertEquals(false, node1.hasChild(node1))
+        assertEquals(false, node1.hasChild(node2))
+        assertEquals(false, node1.hasChild(node3))
+        node1.addChild(link, node2)
+        assertEquals(false, node1.hasChild(node1))
+        assertEquals(true, node1.hasChild(node2))
+        assertEquals(false, node1.hasChild(node3))
+        node1.addChild(link, node3)
+        assertEquals(false, node1.hasChild(node1))
+        assertEquals(true, node1.hasChild(node2))
+        assertEquals(true, node1.hasChild(node3))
+    }
 }
